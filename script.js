1795be9a3d376820b7b09750f1be632e153f8eb9
Kevin Brave <56496351+KevinBrave98@users.noreply.github.com>
2024-02-13 23:52:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function addImage() {
+    document.querySelector(".text").classList.toggle("hide");
+    document.querySelector(".pict").classList.toggle("hide");
+    document.querySelector(".special").classList.toggle("hide");
+}
